1809438571c17cd26ed6835e7ff9e228b8c579ed
Michael <m.pierre@live.ca>
2019-01-07 16:36:01 -0500

    Update smartSensePython.py

--- smartSensePython.py
+++ smartSensePython.py
@@ -2,8 +2,9 @@
 print("Script Started")
 
 print("Writing")
-with open('/etc/sysctl.conf', "w") as sys:
-    for line in sys:
-        if not line.startswith("vm.swappiness"):
-            sys.write(line)
+f = open('/etc/sysctl.conf')
+lines =f.readlines()
+for line in lines:
+    if not line.startswith("vm.swappiness"):
+        sys.write(line)
 print("Complete")
