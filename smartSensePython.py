0b19e70aa0effa958c898ae902c6388d577f3baf
Michael <m.pierre@live.ca>
2019-01-07 16:41:05 -0500

    Update smartSensePython.py

--- smartSensePython.py
+++ smartSensePython.py
@@ -3,8 +3,13 @@ print("Script Started")
 
 print("Writing")
 f = open('/etc/sysctl.conf')
-lines =f.readlines()
+lines = f.readlines()
 for line in lines:
     if not line.startswith("vm.swappiness"):
-        sys.write(line)
+        f.write(line)
+
+f.close()
+f_a = open('/etc/sysctl.conf')
+f_a.write("vm.swappiness = 0")
+f_a.close()
 print("Complete")
