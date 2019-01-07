307e427b4630b956ee80d0ec1c837c861fb8e153
Michael <m.pierre@live.ca>
2019-01-07 16:17:52 -0500

    Update smartSensePython.py

--- smartSensePython.py
+++ smartSensePython.py
@@ -2,18 +2,10 @@ import sys
 print("Script Started")
 
 try:
-    f = open("/etc/sysctl.conf")
     f_a = open("/etc/sysctl.conf", "a")
-    lines = f.readlines
 except FileNotFoundError:
     print("File not found")
     sys.exit()
-
-if 'vm.swappiness = 0' in f.read():
-    print("Setting found and set correctly")
-    f.close()
-else:
-    f.close()
     print("Writing")
     with open('/etc/sysctl.conf') as old_sys, open('/etc/sysctl.conf') as new_sys:
         for line in old_sys:
