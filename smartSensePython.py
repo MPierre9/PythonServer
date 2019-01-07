9d1f4f4a80112a14e60e361525d92dd18a2d4621
Michael <m.pierre@live.ca>
2019-01-07 15:31:43 -0500

    update script

--- smartSensePython.py
+++ smartSensePython.py
@@ -11,9 +11,15 @@ except FileNotFoundError:
 if 'vm.swappiness = 0' in f.read():
     print("Setting found and set correctly")
 elif 'vm.swappiness' in f.read():
-    print("Setting found and set incorrectly")
+    print("Setting found and set incorrectly...")
+    print("Setting parameter")
     for line in lines:
         if not line.startswith('vm.swappiness'):
-            f.write(line)
+            f.write(line) with open("test.txt", "a") as f_a:
+            f_a.write("vm.swappiness = 0")
+
 else:
-    print("Setting not set")
+    print("Setting not found")
+    f_a.write("vm.swappiness = 0")
+
+print("Complete")
