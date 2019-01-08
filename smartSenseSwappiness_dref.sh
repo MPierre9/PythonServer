1eb14d77bd1b1f2701a35cb67113c7fbffa607e4
Michael <m.pierre@live.ca>
2019-01-08 12:20:35 -0500

    Update smartSenseSwappiness_dref.sh

--- smartSenseSwappiness_dref.sh
+++ smartSenseSwappiness_dref.sh
@@ -5,6 +5,10 @@ sysctl -w vm.swappiness=0
 swapoff -a
 swapon -a
 echo 0 > /proc/sys/vm/swappiness
-grep -q "vm.swappiness.*" /etc/sysctl.conf
-	sed -i -e 's/vm.swappiness.*/vm.swappiness = 0/g' /etc/sysctl.conf || echo "vm.swappiness = 0" >> /etc/sysctl.conf
+if grep -q "vm.swappiness.*" /etc/sysctl.conf; then
+	echo "Parameter found."
+	sed -i -e 's/vm.swappiness.*/vm.swappiness = 0/g' /etc/sysctl.conf
+else
+	echo "Parameter not found."
+	echo "vm.swappiness = 0" >> /etc/sysctl.conf
 echo "DONE"
