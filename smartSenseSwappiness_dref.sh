b312e84930b2c3e7f1641f1f19cc67a555a1f40e
Michael <m.pierre@live.ca>
2019-01-08 12:03:50 -0500

    Update smartSenseSwappiness_dref.sh

--- smartSenseSwappiness_dref.sh
+++ smartSenseSwappiness_dref.sh
@@ -5,6 +5,5 @@ sysctl -w vm.swappiness=0
 swapoff -a
 swapon -a
 echo 0 > /proc/sys/vm/swappiness
-sed -i -e '/vm.swappiness.\*/d' /etc/sysctl.conf
-echo 'vm.swappiness = 0' >> /etc/sysctl.conf
+sed -i -e '/vm.swappiness.\*/d' /etc/sysctl.conf && echo 'vm.swappiness = 0' >> /etc/sysctl.conf
 echo "DONE"
