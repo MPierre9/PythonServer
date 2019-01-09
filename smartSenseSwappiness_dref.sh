f57e85ec387e736194649e9b6db12e35aea5967b
Michael <m.pierre@live.ca>
2019-01-09 15:24:08 -0500

    Update smartSenseSwappiness_dref.sh

--- smartSenseSwappiness_dref.sh
+++ smartSenseSwappiness_dref.sh
@@ -7,6 +7,8 @@ echo 10 > /proc/sys/vm/swappiness
 
 # Scan and remove all, if any, instances of vm.swappiness
 sed -i -e '/^\(.*vm.swap.*)$/d' /etc/sysctl.d/*
+sed -i -e '/^\(.*vm.swap.*)$/d' /run/sysctl.d/*
+sed -i -e '/^\(.*vm.swap.*)$/d' /usr/lib/sysctl.d/*
+
 touch /etc/sysctl.d/vm_swappiness.conf
 echo 'vm.swappiness = 10' >> /etc/sysctl.d/vm_swappiness.conf
-echo "DONE"
