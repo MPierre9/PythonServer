449c53de8b15067223b1374250e3cccdac7b8768
Michael <m.pierre@live.ca>
2019-01-10 10:22:48 -0500

    Update increaseSomaxconn.sh

--- increaseSomaxconn.sh
+++ increaseSomaxconn.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
-#
+function SETUP() {
+    echo "SETUP - Complete"
+}
 
-# Reference https://serverfault.com/questions/271380/how-can-i-increase-the-value-of-somaxconn
-echo "Starting Script..."
+# catch STDOUT STDERR cmd args..
+# https://stackoverflow.com/a/41069638/1275256
+catch()
+{
+eval "$({
+__2="$(
+  { __1="$("${@:3}")"; } 2>&1;
+  ret=$?;
+  printf '%q=%q\n' "$1" "$__1" >&2;
+  exit $ret
+  )"
+ret="$?";
+printf '%s=%q\n' "$2" "$__2" >&2;
+printf '( exit %q )' "$ret" >&2;
+} 2>&1 )";
+}
 
-# Temporarily set net to 8192
-echo 8192 > /proc/sys/net/core/somaxconn
+function TEST() {
+    function COMMAND() {
+        sysctl -ap 2>/dev/null | grep net.core.somaxconn # scans all sysctl settings looking for 'net.core.somaxconn'
+    }
+    declare -f COMMAND
+    echo "EXECUTING..."
+    catch out err COMMAND
+    if [[ "$prc" -eq "0" && "$out" =~ "^net.core.somaxconn = ([0-9]+)$" ]] && (( ${BASH_REMATCH[1]} < 8192 ))
+    then
+        echo "FAIL - Bad net.core.somaxconn = ${BASH_REMATCH[1]}. Fixing."
+        # Temporarily set net to 8192
+        echo 8192 > /proc/sys/net/core/somaxconn
 
-# Scan and remove all, if any, instances of net.core/somaxconn
-sed -i -e '/^\.*net.core.somaxconn*$/d' /etc/sysctl.d/*
-sed -i -e '/^\.*net.core.somaxconn*$/d' /usr/lib/sysctl.d/*
+        # Scan and remove all, if any, instances of net.core/somaxconn
+        sed -i -e '/^\.*net.core.somaxconn*$/d' /etc/sysctl.d/*
+        sed -i -e '/^\.*net.core.somaxconn*$/d' /usr/lib/sysctl.d/*
 
-touch /etc/sysctl.d/net_core_somaxconn.conf
-echo 'net.core.somaxconn = 8192' >> /etc/sysctl.d/net_core_somaxconn.conf
-echo 'Complete.'
+        # Create config file with setting
+        touch /etc/sysctl.d/net_core_somaxconn.conf
+        echo 'net.core.somaxconn = 8192' > /etc/sysctl.d/net_core_somaxconn.conf
+    else
+        echo "OK - net.core.somaxconn = ${BASH_REMATCH[@]}"
+    fi
+
+}
+
+function TEARDOWN() {
+    echo "TEARDOWN - Complete"
+}
+
+SETUP
+TEST
+TEARDOWN
