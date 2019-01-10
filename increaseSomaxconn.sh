6c91d969527c10975d369c2589d7001ba2e0af4a
Michael <m.pierre@live.ca>
2019-01-10 10:13:05 -0500

    Update increaseSomaxconn.sh

--- increaseSomaxconn.sh
+++ increaseSomaxconn.sh
@@ -1,46 +1,16 @@
 #!/bin/bash
-function SETUP() {
-    echo "SETUP - Complete"
-}
+#
 
-# catch STDOUT STDERR cmd args..
-# https://stackoverflow.com/a/41069638/1275256
-catch()
-{
-eval "$({
-__2="$(
-  { __1="$("${@:3}")"; } 2>&1;
-  ret=$?;
-  printf '%q=%q\n' "$1" "$__1" >&2;
-  exit $ret
-  )"
-ret="$?";
-printf '%s=%q\n' "$2" "$__2" >&2;
-printf '( exit %q )' "$ret" >&2;
-} 2>&1 )";
-}
+# Reference https://serverfault.com/questions/271380/how-can-i-increase-the-value-of-somaxconn
+echo "Starting Script..."
 
-function TEST() {
-    function COMMAND() {
-        sysctl -ap 2>/dev/null | grep net.core.somaxconn
-    }
-    declare -f COMMAND
-    echo "EXECUTING..."
-    catch out err COMMAND
-    if [[ "$prc" -eq "0" && "$out" =~ "^net.core.somaxconn = ([0-9]+)$" ]] && (( ${BASH_REMATCH[1]} < 8192 ))
-    then
-        echo "FAIL - Bad net.core.somaxconn = ${BASH_REMATCH[1]}. Fixing."
-       #scan all sysctl files comment out any line setting somaxconn, write new file with somaxconn = 8192 with comment
-    else
-        echo "OK - net.core.somaxconn = ${BASH_REMATCH[@]}"
-    fi
+# Temporarily set net to 8192
+echo 8192 > /proc/sys/net/core/somaxconn
 
-}
+# Scan and remove all, if any, instances of net.core/somaxconn
+sed -i -e '/^\.*net.core.somaxconn*$/d' /etc/sysctl.d/*
+sed -i -e '/^\.*net.core.somaxconn*$/d' /usr/lib/sysctl.d/*
 
-function TEARDOWN() {
-    echo "TEARDOWN - Complete"
-}
-
-SETUP
-TEST
-TEARDOWN
+touch /etc/sysctl.d/net_core_somaxconn.conf
+echo 'net.core.somaxconn = 8192' >> /etc/sysctl.d/net_core_somaxconn.conf
+echo 'Complete.'
